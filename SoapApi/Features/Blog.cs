318a22d2c2d25688f19705fd17f64eacef6d39b2
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-08 12:10:23 +0630

    feat: SOAP API

---
+++ SoapApi/Features/Blog.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace SoapApi.Features;
+
+[DataContract]
+public class Blog
+{
+    [DataMember]
+    public int Id { get; set; }
+
+    [DataMember]
+    public string Title { get; set; }
+
+    [DataMember]
+    public string Content { get; set; }
+
+    [DataMember]
+    public DateTime CreatedDate { get; set; }
+}
+
+[ServiceContract]
+public interface IBlogService
+{
+    [OperationContract]
+    void AddBlog(Blog blog);
+
+    [OperationContract]
+    Blog GetBlog(int id);
+
+    [OperationContract]
+    List<Blog> GetAllBlogs();
+
+    [OperationContract]
+    void UpdateBlog(Blog blog);
+
+    [OperationContract]
+    void DeleteBlog(int id);
+}
+
+public class BlogService : IBlogService
+{
+    private static List<Blog> blogs = new List<Blog>();
+
+    public void AddBlog(Blog blog)
+    {
+        blog.Id = blogs.Count + 1;
+        blog.CreatedDate = DateTime.Now;
+        blogs.Add(blog);
+    }
+
+    public Blog GetBlog(int id)
+    {
+        return blogs.FirstOrDefault(b => b.Id == id)!;
+    }
+
+    public List<Blog> GetAllBlogs()
+    {
+        return blogs;
+    }
+
+    public void UpdateBlog(Blog blog)
+    {
+        var existingBlog = blogs.FirstOrDefault(b => b.Id == blog.Id);
+        if (existingBlog != null)
+        {
+            existingBlog.Title = blog.Title;
+            existingBlog.Content = blog.Content;
+        }
+    }
+
+    public void DeleteBlog(int id)
+    {
+        var blog = blogs.FirstOrDefault(b => b.Id == id);
+        if (blog != null)
+        {
+            blogs.Remove(blog);
+        }
+    }
+}
--- SoapApi/Program.cs
+++ SoapApi/Program.cs
@@ -1,4 +1,16 @@
+using SoapApi.Features;
+using SoapCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IBlogService, BlogService>();
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
 app.UseHttpsRedirection();
+
+app.UseSoapEndpoint<IBlogService>("/Blog.asmx", new SoapEncoderOptions());
+app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
